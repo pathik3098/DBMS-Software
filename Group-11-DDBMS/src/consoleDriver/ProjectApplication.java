d820a1676dd0e9f3fc0171c5d12538df0e66126e
DINESH KATAMNENI <dn927347@dal.ca>
2021-02-27 11:11:21 +0530

    Initial Setup-- Added setup files and console driver classes

---
+++ Group-11-DDBMS/src/consoleDriver/ProjectApplication.java
@@ -0,0 +1,10 @@
+package consoleDriver;
+
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		consoleRunner Runner = new consoleRunner();
+		Runner.run();
+	}
+
+}
---
+++ Group-11-DDBMS/src/consoleDriver/consoleRunner.java
@@ -0,0 +1,76 @@
+package consoleDriver;
+
+import java.io.BufferedReader;  
+import java.io.FileReader;  
+import java.io.IOException; 
+import java.util.Scanner;
+
+public class consoleRunner {
+	
+	String name;
+    String pwd;
+	
+	// Method to run via Console
+	public void run() {
+
+		// Initializing variables
+		String commandEntered;
+		Scanner userInput = new Scanner(System.in);
+		String quitCommand = "quit";
+		String adminUsername;
+		String adminPassword;
+		
+		System.out.println("Welcome");
+		System.out.println("1. To login ");
+		System.out.println("quit. To exit the application");
+		System.out.println("Choose 1 or quit");
+		
+		do {
+			// First call the login function and login as customer or administrator
+			commandEntered = userInput.next();
+			if(commandEntered.equalsIgnoreCase("1")) {
+				//logic for user authentication
+				System.out.println("Please enter your credentials");
+		        System.out.println("Enter your username:");
+		        adminUsername = userInput.next();
+		        System.out.println("Enter your password:");
+		        adminPassword = userInput.next();
+		
+		    	String line = "";
+		    	String splitBy = ",";
+		    	
+		    	try   
+		    	{  
+		    	BufferedReader br = new BufferedReader(new FileReader("credentials.csv"));  
+		    	while ((line = br.readLine()) != null)   
+		    	{  
+		    	String[] user = line.split(splitBy);
+		    	name = user[0];
+		    	pwd = user[1];
+		    	if(adminUsername.equals(name) && adminPassword.equals(pwd)) {
+					System.out.println("Login was successful.");
+					
+				   }
+		    	else {
+		    		System.out.println("Incorrect Login");
+		    	}
+		    	break;
+		    	}
+		    	br.close();
+		    	}   
+		    	catch (IOException e)   
+		    	{  
+		    	System.out.println(e.getMessage()); 
+		    	}    
+			}
+			else if(commandEntered.equalsIgnoreCase("quit")) {
+				System.out.println("Exiting the application.");
+			}
+			else {
+				System.out.println("Bad Command, Please enter a valid input - 1 or quit");
+			}
+	}while(!commandEntered.equalsIgnoreCase(quitCommand));
+	userInput.close();
+	}
+
+}
