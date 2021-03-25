cfc56dff986fd9c2468dd07e52079e3c6eee8c34
DINESH KATAMNENI <dn927347@dal.ca>
2021-03-25 15:44:41 +0530

    Added credentials.csv file

--- Group-11-DDBMS/src/parser/DeleteRegex.java
+++ Group-11-DDBMS/src/parser/DeleteRegex.java
@@ -1,4 +1,4 @@
-package com.group11;
+package parser;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
--- Group-11-DDBMS/src/parser/InsertRegex.java
+++ Group-11-DDBMS/src/parser/InsertRegex.java
@@ -1,4 +1,4 @@
-package com.group11;
+package parser;
 
 import java.util.regex.*;
 
