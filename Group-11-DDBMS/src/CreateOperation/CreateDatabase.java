e9a29f097aadfe6f38029d5c022cc22ecfef2562
Pathik Kumar Patel <pt999986@dal.ca>
2021-03-28 20:38:25 +0530

    Added Create Database Query

---
+++ Group-11-DDBMS/src/CreateOperation/CreateDatabase.java
@@ -0,0 +1,34 @@
+package CreateOperation;
+
+import java.io.File;
+
+public class CreateDatabase {
+    String databaseName;
+    String path = "D:\\Database";
+
+    String[] statement;
+
+    public CreateDatabase(String[] s){
+        statement = s;
+        databaseName = statement[2];
+        path = path+"\\"+ databaseName;
+        System.out.println(path);
+    }
+
+    public void createDatabase(){
+        File file = new File(path);
+        boolean bool = file.mkdir();
+
+        if(bool)
+        {
+            System.out.println("Database succesfully created");
+        }
+
+        else{
+            System.out.println("Error in creating database");
+        }
+
+    }
+
+
+}
---
+++ Group-11-DDBMS/src/CreateOperation/CreateMain.java
@@ -0,0 +1,15 @@
+package CreateOperation;
+
+public class CreateMain {
+    public static void main(String args[]){
+        String [] createDatabaseStatement = new String[3];
+        createDatabaseStatement[0]="CREATE";
+        createDatabaseStatement[1]="DATABASE";
+        createDatabaseStatement[2]="TestDatabase";
+
+        CreateDatabase d = new CreateDatabase(createDatabaseStatement);
+
+        d.createDatabase();
+
+    }
+}
