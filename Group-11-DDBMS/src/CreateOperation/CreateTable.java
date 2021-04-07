316a5b62bad2edd604c8c021bc99e6f802aa5f3e
Pathik Kumar Patel <pt999986@dal.ca>
2021-04-07 17:15:47 +0530

    added pk and fk in metadata

--- Group-11-DDBMS/src/CreateOperation/CreateTable.java
+++ Group-11-DDBMS/src/CreateOperation/CreateTable.java
@@ -3,6 +3,10 @@ package CreateOperation;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -13,14 +17,15 @@ public class CreateTable {
     ArrayList<String> queryTokens = new ArrayList<String>();
     String path = currentDB;
     String tablemetapath,tablepath;
+    String [] getDataInsideBracket;
     boolean result;
 
     public CreateTable(String query)
     {
         this.query = query;
         tokenize();
-        tablemetapath = path+"\\"+queryTokens.get(2)+"meta"+".txt";
-        tablepath = path+"\\"+ queryTokens.get(2)+".txt";
+        tablemetapath = path+"\\"+queryTokens.get(2).toLowerCase()+"meta"+".txt";
+        tablepath = path+"\\"+ queryTokens.get(2).toLowerCase()+".txt";
     }
 
     private void tokenize()
@@ -33,6 +38,7 @@ public class CreateTable {
         String intermediateResult = getPartInsideRoundBracket[1];
         String[] removeLastCurlyBracket = intermediateResult.split("\\)");
         String[] getColumnTokens = removeLastCurlyBracket[0].split(",");
+        getDataInsideBracket = removeLastCurlyBracket[0].split(",");
 
         for(String j : getColumnTokens)
         {
@@ -43,16 +49,32 @@ public class CreateTable {
 
     public void execute()
     {
-        createFile(tablemetapath);
+        File file = new File(tablemetapath);
+        try
+        {
+            result = file.createNewFile();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
 
         try
         {
             if(result)
             {
                 FileWriter myWriter = new FileWriter(tablemetapath);
-                for(int i = 3; i< queryTokens.size(); i=i+2)
+                for(int i = 0; i< getDataInsideBracket.length; i++)
                 {
-                    myWriter.write(queryTokens.get(i)+"-"+queryTokens.get(i+1)+"\n");
+                    Integer a = i;
+                    String index = a.toString();
+                    myWriter.write(index);
+                    String[] abc = getDataInsideBracket[i].split(" ");
+                    for(String t: abc)
+                    {
+                        myWriter.write("-"+t);
+                    }
+                    myWriter.write("\n");
                 }
                 myWriter.close();
             }
@@ -63,14 +85,10 @@ public class CreateTable {
             e.printStackTrace();
         }
 
-        createFile(tablepath);
-    }
-
-    private void createFile(String filePath) {
-        File file = new File(filePath);
+        File createFile = new File(tablepath);
         try
         {
-            result = file.createNewFile();
+            result = createFile.createNewFile();
             if(result)
             {
                 System.out.println("table succesfully created");
@@ -83,6 +101,15 @@ public class CreateTable {
         catch (IOException e)
         {
             e.printStackTrace();
+        }
+
+        String dumpFilePath = currentDB+"/"+"SQLdump.txt";
+        Path path = Paths.get(dumpFilePath);
+
+        try {
+            Files.writeString(path, query, StandardCharsets.UTF_8);
+        } catch (IOException ex) {
+            System.out.println("Error inserting create query in sql dump file");
         }
     }
 }
