38285c83f0c2ab16a27cd76e8e6becca7d9892c6
Pathik Kumar Patel <pt999986@dal.ca>
2021-04-07 19:33:15 +0530

    added foreign key constraints

--- Group-11-DDBMS/src/CreateOperation/CreateTable.java
+++ Group-11-DDBMS/src/CreateOperation/CreateTable.java
@@ -7,6 +7,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -16,22 +17,20 @@ public class CreateTable {
     String query;
     ArrayList<String> queryTokens = new ArrayList<String>();
     String path = currentDB;
-    String tablemetapath,tablepath;
-    String [] getDataInsideBracket;
+    String tablemetapath, tablepath;
+    String[] getDataInsideBracket;
     boolean result;
 
-    public CreateTable(String query)
-    {
+    public CreateTable(String query) {
         this.query = query;
         tokenize();
-        tablemetapath = path+"\\"+queryTokens.get(2).toLowerCase()+"meta"+".txt";
-        tablepath = path+"\\"+ queryTokens.get(2).toLowerCase()+".txt";
+        tablemetapath = path + "\\" + queryTokens.get(2).toLowerCase() + "meta" + ".txt";
+        tablepath = path + "\\" + queryTokens.get(2).toLowerCase() + ".txt";
     }
 
-    private void tokenize()
-    {
-        String[] getPartBeforeRoundBracket=query.split("\\((.*?)\\);");
-        String[]  getTableName= getPartBeforeRoundBracket[0].split(" ");
+    private void tokenize() {
+        String[] getPartBeforeRoundBracket = query.split("\\((.*?)\\);");
+        String[] getTableName = getPartBeforeRoundBracket[0].split(" ");
         queryTokens.addAll(Arrays.asList(getTableName));
 
         String[] getPartInsideRoundBracket = query.split("\\(");
@@ -40,74 +39,99 @@ public class CreateTable {
         String[] getColumnTokens = removeLastCurlyBracket[0].split(",");
         getDataInsideBracket = removeLastCurlyBracket[0].split(",");
 
-        for(String j : getColumnTokens)
-        {
+        for (String j : getColumnTokens) {
             String[] abc = j.split(" ");
             queryTokens.addAll(Arrays.asList(abc));
         }
     }
 
-    public void execute()
+    public String foreignKeyTableName() {
+        int length = getDataInsideBracket.length;
+        length = length-1;
+        String lastRow = getDataInsideBracket[length];
+        String foreignKeyTableName = null;
+
+        if (lastRow.toUpperCase().contains("FK")) {
+            String[] foreignKey = lastRow.split(" ");
+            foreignKeyTableName = foreignKey[3].toLowerCase();
+        }
+        System.out.println(foreignKeyTableName);
+        return foreignKeyTableName;
+    }
+
+    public boolean checkIfForeignTableExists(String fTableName) {
+        return Files.exists(Paths.get(currentDB + "/" + fTableName + ".txt"));
+    }
+
+    public void execute() {
+
+        String foreignTableName = foreignKeyTableName();
+
+        if(foreignTableName == null)
+        {
+            createTableOperation();
+        }
+
+        else
+        {
+            boolean checkForeignTable = checkIfForeignTableExists(foreignTableName);
+            if(checkForeignTable)
+            {
+                createTableOperation();
+            }
+            else
+            {
+                System.out.println("Foreign Table doesn't");
+            }
+        }
+    }
+
+    public void createTableOperation()
     {
         File file = new File(tablemetapath);
-        try
-        {
+        try {
             result = file.createNewFile();
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
-        try
-        {
-            if(result)
-            {
+        try {
+            if (result) {
                 FileWriter myWriter = new FileWriter(tablemetapath);
-                for(int i = 0; i< getDataInsideBracket.length; i++)
-                {
+                for (int i = 0; i < getDataInsideBracket.length; i++) {
                     Integer a = i;
                     String index = a.toString();
                     myWriter.write(index);
                     String[] abc = getDataInsideBracket[i].split(" ");
-                    for(String t: abc)
-                    {
-                        myWriter.write("-"+t);
+                    for (String t : abc) {
+                        myWriter.write("-" + t);
                     }
                     myWriter.write("\n");
                 }
                 myWriter.close();
             }
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
 
         File createFile = new File(tablepath);
-        try
-        {
+        try {
             result = createFile.createNewFile();
-            if(result)
-            {
+            if (result) {
                 System.out.println("table succesfully created");
-            }
-            else
-            {
+            } else {
                 System.out.println("table already exist");
             }
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
-        String dumpFilePath = currentDB+"/"+"SQLdump.txt";
+        String dumpFilePath = currentDB + "/" + "SQLdump.txt";
         Path path = Paths.get(dumpFilePath);
 
         try {
-            Files.writeString(path, query, StandardCharsets.UTF_8);
+            Files.writeString(path, query + "\n", StandardCharsets.UTF_8, StandardOpenOption.APPEND);
         } catch (IOException ex) {
             System.out.println("Error inserting create query in sql dump file");
         }
