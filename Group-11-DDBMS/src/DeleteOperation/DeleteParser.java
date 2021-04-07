1323388027519723117d66248c794839565eaaeb
DINESH KATAMNENI <dn927347@dal.ca>
2021-04-07 19:33:23 +0530

    added logging

--- Group-11-DDBMS/src/DeleteOperation/DeleteParser.java
+++ Group-11-DDBMS/src/DeleteOperation/DeleteParser.java
@@ -1,47 +1,42 @@
 package DeleteOperation;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 import java.io.IOException;
 
 public class DeleteParser {
 
-    public void parseQuery(String query) throws IOException {
+    private static Logger log = LogManager.getLogger(DeleteParser.class);
+
+    public boolean parseQuery(String query) throws IOException {
+        log.info("Parsing the Delete query");
         DeleteQuery delete = new DeleteQuery();
         String tableName = fetchTableName(query);
         String conditionColumnName = getColumnName(query);
         String conditionColumnValue = getColumnValue(query);
         delete.executeQuery(tableName,conditionColumnName,conditionColumnValue);
+        return true;
     }
 
     public String fetchTableName(String query) {
 
-//        int positionOfFrom = query.toLowerCase().indexOf("from");
-//        int positionOfSpace = query.indexOf(" ",positionOfFrom + 1);
-//        int positionOfWhere = query.toLowerCase().indexOf("where",positionOfSpace + 1);
-//        return query.substring(positionOfSpace + 1,positionOfWhere).toLowerCase().trim();
-
-        int indexOfFrom = query.toLowerCase().indexOf("from");
-        int indexOfSpace = query.indexOf(" ",indexOfFrom + 1);
+        int positionOfFrom = query.toLowerCase().indexOf("from");
+        int positionOfSpace = query.indexOf(" ",positionOfFrom + 1);
         if (query.toLowerCase().contains("where")) {
-            int indexOfWhere = query.toLowerCase().indexOf("where",indexOfSpace + 1);
-            return query.substring(indexOfSpace + 1,indexOfWhere).toLowerCase().trim();
+            int indexOfWhere = query.toLowerCase().indexOf("where",positionOfSpace + 1);
+            return query.substring(positionOfSpace + 1,indexOfWhere).toLowerCase().trim();
         }
-        return query.substring(indexOfSpace + 1, query.length()).toLowerCase().trim();
+        return query.substring(positionOfSpace + 1, query.length()).toLowerCase().trim();
     }
 
     public String getColumnName(String query)
     {
-//            int positionOfWhere = query.toLowerCase().indexOf("where");
-//            int positionOfSpaceAfterWhere = query.indexOf(" ", positionOfWhere + 1);
-//            int positionOfEqual = query.indexOf("=", positionOfSpaceAfterWhere);
-//            String columnName = query.substring(positionOfSpaceAfterWhere + 1, positionOfEqual).toLowerCase().trim();
-//            return columnName;
-
         if (query.contains("where")) {
             int positionOfWhere = query.toLowerCase().indexOf("where");
             int positionOfSpaceAfterWhere = query.indexOf(" ", positionOfWhere + 1);
             int positionOfEqual = query.indexOf("=", positionOfSpaceAfterWhere);
-            String columnName = query.substring(positionOfSpaceAfterWhere + 1, positionOfEqual).toLowerCase().trim();
-            return columnName;
+            return query.substring(positionOfSpaceAfterWhere + 1, positionOfEqual).toLowerCase().trim();
         }
         return null;
     }
@@ -50,8 +45,7 @@ public class DeleteParser {
     {
         if (query.contains("where")) {
             int positionOfEqual = query.indexOf("=");
-            String columnValue = query.substring(positionOfEqual + 1, query.length()).trim();
-            return columnValue;
+            return query.substring(positionOfEqual + 1, query.length()).trim();
         }
         return null;
     }
